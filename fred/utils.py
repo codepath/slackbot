f0ad6e0125008a8e5e217c3c5cf4363b79ad0921
Jonathan Como <jonathan.como@gmail.com>
2017-04-25 19:47:48 -0700

    Move templates to top level

--- fred/utils.py
+++ fred/utils.py
@@ -2,7 +2,7 @@ from jinja2 import Template
 
 
 def render_template(template_id, **kwargs):
-    with open('fred/templates/{}.slack'.format(template_id)) as f:
+    with open('templates/{}.slack'.format(template_id)) as f:
         template = Template(''.join(f.readlines()))
 
     return template.render(**kwargs)
